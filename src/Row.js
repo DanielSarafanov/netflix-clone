083148b0d948f5d5faeab51b63b63f9b5fe34324
Daniel Sarafanov <sarafanovdaniel@gmail.com>
2024-01-17 19:21:45 -0500

    Row CSS styling and fetch data from API

--- src/Row.js
+++ src/Row.js
@@ -5,6 +5,7 @@ import axios from './axios';
 function Row({title, fetchUrl, isLargeRow = false}) {
 
     const [movies, setMovies] = useState([]);
+    const image_base_url = 'https://image.tmdb.org/t/p/original/'
 
     useEffect(() => {
         async function fetchData(){
@@ -18,8 +19,13 @@ function Row({title, fetchUrl, isLargeRow = false}) {
   return (
     <div className='row'>
         <h2>{title}</h2>
+        <div className='row_posters'>
+            {movies.map(movie => ( (isLargeRow && movie.poster_path || !isLargeRow && movie.poster_path) && (
+                <img key={movie.id} className ={`row_poster ${isLargeRow && 'row_posterLarge'}`} src={`${image_base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt='Row Info' />)
+            ))}
+        </div>
     </div>
   )
 }
 
-export default Row
+export default Row;
