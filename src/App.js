31b7c1fefb0cd5efdceb8d3d8d7d32e277ba4906
Daniel Sarafanov <sarafanovdaniel@gmail.com>
2024-01-18 17:46:17 -0500

    Add Login Screen Router Logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
-import Homescreen from './Homescreen';
+import Homescreen from './screens/Homescreen/Homescreen';
+import LoginScreen from './screens/Login/Login'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
+
+  const user= null;
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: user ? <Homescreen /> : <LoginScreen />
+    },
+    // Add more routes here as needed
+  ]);
+
   return (
     <div className="app">
-        <Homescreen />
+        
+        <RouterProvider router={router} />
+      
     </div>
   );
 }
